00f20138ac1e91537849acaadb68a49d8c3cfd45
Mohan Kumar <mohanjhd5319@gmail.com>
2025-08-08 15:53:10 +0530

    case fixing

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const blackListTokenModel = require('../models/blackListToken.model');
 const captainModel = require('../models/captain.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 
 
 module.exports.authUser = async (req, res, next) => {
@@ -41,7 +41,7 @@ module.exports.authCaptain = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+    const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
 
 
 
