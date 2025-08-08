3f1a059c60bac994ff1a79dc4aaeca0574420911
Mohan Kumar <mohanjhd5319@gmail.com>
2025-08-08 15:09:07 +0530

    fix for production

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 initializeSocket(server);
+const PORT = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
